b8e056f3a05d120e3eae3bf0e185a7354cda23ca
gagan sidhu <gagan@hotmail.com>
2023-09-24 21:58:35 -0600

    [4.14.326/53528] update a bunch of programs and shit
    -coreutils updated
    -unbound
    -tor
    -mc
    -dnscrypt resolvers
    -brainslayer boner special aka frr
    -update groff
    -update less
    
    i dunno man a lot of things. i am busy with other shit rn but i thought
    this would be a good thing to do while i remember.

---
+++ lootbag/usr/local/share/doc/groff-1.23.0/examples/mm/letter.mm
@@ -0,0 +1,51 @@
+.\" Set a blank page header; our letter is a single page and does not
+.\" require the default numbering.
+.PH '''
+.\" Set point size to 14.
+.S 14
+.\" Set paragraph type to "indented".
+.nr Pt 1
+.\" Put the date in a right-aligned display.
+.DS R
+30 May 2021
+.DE
+.\" Start display (left-aligned).
+.DS
+Mr. Ty Coon
+President of Vice, Intellectual Property and Licensing
+Very Big Corporation of America
+Silly Valley, CA 94043
+.\" End display.
+.DE
+Dear Mr. Coon,
+.
+.
+.\" Start new paragraph.
+.P
+I would like to request that your wholly-owned subsidiary,
+Yoyodyne, Inc.,
+disclaim all copyright interest in the program `Gnomovision'
+(which makes passes at compilers)
+written by Juliet Hacker prior to her employment with Yoyodyne.
+.
+.
+.P
+Our colleagues at the Software Freedom Conservancy have determined and
+informed us that Ms.\& Hacker's employment agreement is a contract of
+adhesion.
+.
+.
+.P
+Thank you for your prompt attention to this matter.
+.
+.
+.\" Put the closing and signature in a right-aligned display as well.
+.DS R
+Sincerely yours,
+.\" Leave three "vees" (3 text line heights) of room for signature.
+.SP 3v
+.
+.
+Tracy T.\& Jordan
+.DE
+.\" vim: set noexpandtab textwidth=72:
